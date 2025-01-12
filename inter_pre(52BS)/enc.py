22416259c1e2702adb89ee5ffd18a9cb4df9571b
winter_NaN <weizgoole@gmail.com>
2025-01-12 20:36:03 +0800

    whole frame prediction and coding(issue)

--- inter_pre(52BS)/enc.py
+++ inter_pre(52BS)/enc.py
@@ -4,112 +4,150 @@ import math
 import csv
 import os
 
- # 假设第一个帧不编码，直接使用原始值
-previous_reconstructed_frame = [0] * 52
-
+# 帧内第一帧初始重建
+rec_intra_frame = [0] * 52
+# 帧间第一帧初始重建值
+rec_inter_frame = [0] * 52
+# 参考映射表
+referenceTable = [
+        [0, 1, 2,  3, 4,  5,  6,  15, 16, 18, 19, 20, 21, 22, 23, 25, 27, 29, 31, 32, 33, 35, 37, 39, 41, 44, 46, 47, 49, 51],
+        [7, 8, 10, 9, 11, 12, 13, -1, -1, -1, -1, -1, -1, -1, 24, 26, 28, 30, -1, -1, 34, 36, 38, 40, 42, 45, -1, 48, 50, -1],
+        [-1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 14, -1, -1, -1, -1, 17, -1, -1, 43, -1, -1, -1, -1]
+    ]
 # 帧内预测
-def predict():
-    pass
+def intra_prediction(frames, rec_intra_frame):
+    res_frames = ['none'] * 52
+    for i in range(len(frames)):
+        if   i in referenceTable[0]:
+            res_frames[i] = frames[i]
+    for i in range(len(frames)):
+        if   i in referenceTable[1]:
+            res_frames[i] = frames[i] - rec_intra_frame[referenceTable[0][i]]
+    for i in range(len(frames)):
+        if   i in referenceTable[2]:
+            res_frames[i] = frames[i] - rec_intra_frame[referenceTable[0][i]]    
+    for i in range(len(res_frames)):
+        if res_frames[i] == 'none':
+            print("error: intra_prediction, reference mapping error")
+    
+    return res_frames   
+# 帧内重建
+def rec_intra_prediction(quantized_residual, quantization_step):
+    rec_intra_frame = ['none'] * 52
+    for i, value in quantized_residual:
+        if   i in referenceTable[0]:
+            if value % 2 == 0:
+                value = int((-value) / 2)
+            else:
+                value = int((value + 1) / 2)
+            rec_intra_frame[i] =  value * quantization_step
+    for i, value in quantized_residual:
+        if   i in referenceTable[1]:
+            if value % 2 == 0:
+                value = int((-value) / 2)
+            else:
+                value = int((value + 1) / 2)
+            rec_intra_frame[i] =  value * quantization_step + rec_intra_frame[referenceTable[0][i]]     
+    for i, value in quantized_residual:
+        if   i in referenceTable[2]:
+            if value % 2 == 0:
+                value = int((-value) / 2)
+            else:
+                value = int((value + 1) / 2)
+            rec_intra_frame[i] =  value * quantization_step + rec_intra_frame[referenceTable[0][i]] 
+    return rec_intra_frame
 
 # 帧间预测
 def inter_prediction(current_frame, previous_reconstructed_frame):
-    return int(current_frame - previous_reconstructed_frame)
+    residual = []
+    for i in range(len(current_frame)):
+        residual.append(int(current_frame[i]- previous_reconstructed_frame[i])) 
+    return residual
+# 帧间重建
+def rec_inter_prediction(quantized_residual, quantization_step):
+    for i, value in quantized_residual:
+            if value % 2 == 0:
+                value = int((-value) / 2)
+            else:
+                value = int((value + 1) / 2)
+
+            rec_inter_frame[i] = rec_inter_frame[i] + value * quantization_step
+    return rec_inter_frame
 
 # 数据偏移
 
+
 # 量化残差
 def quantize(data, quantization_step):
-    data = np.round(data / quantization_step).astype(int)
-    if data <= 0:
-        data = (-data) * 2
-    else:
-        data = data * 2 - 1
-    return data
+    quantized_data = []
+    for value in data:
+        rounded_value = np.round(value / quantization_step)
+        int_value = int(rounded_value)
+        if int_value > 0:
+            # 正数变为正奇数
+            quantized_value = int_value * 2 - 1
+        else:
+            quantized_value = (-int_value) * 2
+        quantized_data.append(quantized_value)
+    return quantized_data
 
 
 # 指数哥伦布编码
 def exponential_golomb_encode(values):
-    # def test_exponential_golomb_encode(n):
-    unarycode = ''
-    golombCode = ''
+    frame = ''
+    for value in values:
+        unarycode = ''
+        golombCode = ''
+        groupID = np.floor(np.log2(value + 1))
+        temp_ = groupID
+        # print(groupID)
 
-    # values = np.array(values)  # 将列表转换为 NumPy 数组
-    ###Quotient and Remainder Calculation
-    groupID = np.floor(np.log2(values + 1))
-    temp_ = groupID
-    # print(groupID)
+        while temp_ > 0:
+            unarycode = unarycode + '0'
+            temp_ = temp_ - 1
+        unarycode = unarycode  # +'1'
 
-    while temp_ > 0:
-        unarycode = unarycode + '0'
-        temp_ = temp_ - 1
-    unarycode = unarycode  # +'1'
+        index_binary = bin(value + 1).replace('0b', '')
+        golombCode = unarycode + index_binary
 
-    index_binary = bin(values + 1).replace('0b', '')
-    golombCode = unarycode + index_binary
-    return golombCode
+        frame = frame + golombCode
+    return frame
 
 
 def encode_frames(frames, quantization_step, filename, inter_flag, intra_flag):
     with open(filename, 'ab+') as f:
         # f.write(struct.pack('d', frames[0]))  # 负数
         bitstream = ''
+        rec_intra_frame = frames
 
-        for i in range(len(frames)):
-            if intra_flag:
-                
-                if inter_flag:
-                    # 帧间预测
-                    residual = inter_prediction(frames[i], previous_reconstructed_frame[i])
-                else:
-                    pass
-                    # 
-            else:
-                
-                if inter_flag:
-                    # 帧间预测
-                    residual = inter_prediction(frames[i], previous_reconstructed_frame[i])
-                else:
-                    residual = frames[i]
+        if intra_flag:
+            frames = intra_prediction(frames, rec_intra_frame)
+        else:
+            pass 
+      
+        if inter_flag:
+            residual = inter_prediction(frames, rec_inter_frame)
+        else:
+            residual = frames
+             
+        # 量化残差
+        quantized_residual = quantize(residual, quantization_step)
+        # 指数哥伦布编码
+        encoded_residual = exponential_golomb_encode(quantized_residual)
+        bitstream += encoded_residual
             
-           
-            # 量化残差
-            quantized_residual = quantize(residual, quantization_step)
-            # 指数哥伦布编码
-            encoded_residual = exponential_golomb_encode(quantized_residual)
-            bitstream += encoded_residual
-            
-            # 重建当前帧
-            if intra_flag:
+        # 帧间重建   
+        if inter_flag:
+            # 重建下一帧
+            rec_inter_frame = rec_inter_prediction(quantized_residual, quantization_step)  
+        else:
+            pass
+        
+        if intra_flag:
+            rec_intra_frame = rec_intra_prediction(quantized_residual, quantization_step)
+        else:
+            pass 
                 
-                if inter_flag:
-                    # 重建下一帧
-                    if quantized_residual % 2 == 0:
-                        quantized_residual = int((-quantized_residual) / 2)
-                    else:
-                        quantized_residual = int((quantized_residual + 1) / 2)
-
-                    # if i == 0:
-                    #     previous_reconstructed_frame[i] = 0 + quantized_residual * quantization_step
-                    # else:
-                    previous_reconstructed_frame[i] = previous_reconstructed_frame[i] + quantized_residual * quantization_step
-                else:
-                    pass
-            else:
-                
-                if inter_flag:
-                    # 重建下一帧
-                    if quantized_residual % 2 == 0:
-                        quantized_residual = int((-quantized_residual) / 2)
-                    else:
-                        quantized_residual = int((quantized_residual + 1) / 2)
-
-                    # if i == 0:
-                    #     previous_reconstructed_frame = 0 + quantized_residual * quantization_step
-                    # else:
-                    previous_reconstructed_frame[i] = previous_reconstructed_frame[i] + quantized_residual * quantization_step
-                else:
-                    pass
-            # 确保 bitstream 的长度是8的倍数
 
         while len(bitstream) % 8 != 0:
             bitstream += '0'
@@ -128,10 +166,7 @@ def encode_frames(frames, quantization_step, filename, inter_flag, intra_flag):
         #     print("error!!!")
         f.write(byte_array)
 
-# 左右预测
-def predict_LeftR():
-    pass
-
+        f.close()
 
 def encode_from_csv(input_folder, output_folder, QP, n=6, inter_flag=0, intra_flag=0):
     
